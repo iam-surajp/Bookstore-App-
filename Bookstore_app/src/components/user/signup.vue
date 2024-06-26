9c5184ad055bba9286d3eb258dbc19209079fee5
Suraj <psuraj31413@gmail.com>
2024-06-26 11:04:02 +0530

    [Suraj] Add: Created signup page ui for user signup

---
+++ Bookstore_app/src/components/user/signup.vue
@@ -0,0 +1,78 @@
+<script lang="ts">
+import Login from "./login.vue";
+
+export default {
+  name: "Signup",
+  data() {
+    return {
+      tabs: null,
+    };
+  },
+  components: {
+    Login,
+  },
+};
+</script>
+
+<template>
+  <v-card class="mx-auto" width="400px">
+    <v-tabs v-model="tabs" color="primary" grow>
+      <v-tab :value="1"> Login </v-tab>
+
+      <v-tab :value="2"> Signup </v-tab>
+    </v-tabs>
+
+    <v-tabs-window v-model="tabs" style="width: 100%; height: 100%">
+      <v-tabs-window-item v-for="i in 2" :key="i" :value="i">
+        <div v-if="i === 2" style="width: 75%; margin: auto">
+          <div class="fields">
+            <div class="labels">Full Name</div>
+            <v-text-field density="compact" variant="outlined"></v-text-field>
+          </div>
+          <div class="fields">
+            <div class="labels">Email id</div>
+            <v-text-field density="compact" variant="outlined"></v-text-field>
+          </div>
+          <div class="fields">
+            <div class="labels">Password</div>
+            <v-text-field density="compact" variant="outlined"></v-text-field>
+          </div>
+          <div class="fields">
+            <div class="labels">Mobile Number</div>
+            <v-text-field density="compact" variant="outlined"></v-text-field>
+          </div>
+          <div style="width: 100%; margin: auto">
+            <v-btn class="btn">Signup</v-btn>
+          </div>
+        </div>
+        <div v-else style="width: 75%; margin: auto">
+          <Login />
+        </div>
+      </v-tabs-window-item>
+    </v-tabs-window>
+  </v-card>
+</template>
+
+<style scoped>
+.v-card {
+  height: 500px;
+}
+
+.fields {
+  width: 100%;
+  height: 70px;
+  margin: 10px;
+}
+
+.labels {
+  font-size: 13px;
+}
+
+.btn {
+  width: 100%;
+  background-color: darkred;
+  color: white;
+  text-transform: capitalize;
+  margin: auto;
+}
+</style>
--- Bookstore_app/src/router/index.ts
+++ Bookstore_app/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Signup from '@/components/user/signup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,13 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+
+    {
+      path:'/signup',
+      name:'signup',
+      component:Signup
+    },
+
     {
       path: '/about',
       name: 'about',
